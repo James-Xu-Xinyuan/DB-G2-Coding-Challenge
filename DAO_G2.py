262629e9a9dd2ba002adf075af205ec9c22c4620
Xinyuan Xu <47001686+James-Xu-Xinyuan@users.noreply.github.com>
2020-10-22 17:36:50 +0100

    Add files via upload
    Caution: not tested and debugged yet!
    user_auth(username, password): return boolean for login
    get_instrument_latest_avg(instrument_name): return (avg_sell, avg_buy)
    def get_latest_deals(): return list of data

---
+++ DAO_G2.py
@@ -0,0 +1,83 @@
+import mysql.connector
+
+
+def user_auth(username, password):
+
+    cnx = mysql.connector.connect(user='root', password='ppp', host='127.0.0.1',database='db_grad_cs_1917')
+    cursor1 = cnx.cursor()
+    query = ("SELECT user_pwd FROM users WHERE user_id='" + str(username) + "'")
+    cursor1.execute(query)
+    response = cursor1.fetchone()
+
+    if response:
+        user_pwd = response[0]
+        if user_pwd == password:
+            cursor1.close()
+            return True
+        else:
+            print("invalid username or password")
+            cursor1.close()
+            return False
+        # potentially add login record
+    else:
+        print("invalid username or password")
+        cursor1.close()
+        return False
+
+
+
+# get the latest and aggregate deal data for an instrument
+def get_instrument_latest_avg(instrument_name):
+
+    cnx = mysql.connector.connect(user='root', password='ppp', host='127.0.0.1',database='db_grad_cs_1917')
+    cursor2 = cnx.cursor()
+    cursor3 = cnx.cursor()
+
+    query = ("SELECT instrument_id FROM instrument WHERE instrument_name='" + instrument_name + "'")
+    cursor2.execute(query)
+    response = cursor1.fetchone()
+    if response:
+        instrument_id = response[0]
+        print(instrument_id)
+    else:
+        cursor2.close()
+        return "cant find this instrument"
+    
+    query = ("SELECT MAX(deal_id) FROM deal ")
+    cursor3.execute(query)
+    latest_deal_id = cursor3.fetchone()[0] - 100
+    print(latest_deal_id)
+
+    query = ("SELECT AVG(deal_amount) FROM deal" + " WHERE deal_type='B' " + 
+             " AND deal_instrument_id="+str(instrument_id) +"AND deal_id>" + str(latest_deal_id))
+    cursor2.execute(query)
+    avg_buy = cursor2.fetchone()[0]
+    print(avg_buy)
+
+    query = ("SELECT AVG(deal_amount) FROM deal" + " WHERE deal_type='S' " + 
+             " AND deal_instrument_id="+str(instrument_id) +"AND deal_id>" + str(latest_deal_id))
+    cursor3.execute(query)
+    avg_sell = cursor3.fetchone()[0]
+    print(avg_sell)
+
+    return (avg_sell, avg_buy)
+
+
+# get the lastest (approximatly 10 to 20) deals from databse
+def get_latest_deals():
+    cnx = mysql.connector.connect(user='root', password='ppp', host='127.0.0.1',database='db_grad_cs_1917')
+    cursor4 = cnx.cursor()
+    cursor5 = cnx.cursor()
+    deal_data = []
+
+    query = ("SELECT MAX(deal_id) FROM deal ")
+    cursor4.execute(query)
+    latest_deal_id = cursor4.fetchone()[0] - 10
+    print(latest_deal_id)
+    query = ("SELECT * FROM deal WHERE deal_id>" + str(latest_deal_id) )
+    cursor5.execute(query)
+    # how should return data be formated
+    for row in cursor5:
+        deal_data.append(row)
+        
+    return 
