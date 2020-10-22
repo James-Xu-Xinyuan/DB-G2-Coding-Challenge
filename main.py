05bd8df3a792fbacf65c90d9da7a15649c3bef41
Amina <aminaxnavas@gmail.com>
2020-10-22 10:48:41 +0100

    amina, zekun: connect sign in page with dashboard, did basic empty input error handling and also tested connection to the other layers

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+import json
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template("sign-in-page.html")
+
+
+@app.route('/dash', methods=['GET', 'POST'])
+def dashboard():
+    if request.method == "POST":
+        username = request.json['username']
+        password = request.json['password']
+
+    return render_template("dashboard.html")
+
+
+@app.route('/testConnection')
+def testConn():
+    # r = requests.get('http://localhost:8090/client/testservice')
+    # return Response(r.iter_lines(chunk_size=1), mimetype="text/json")
+
+    r = requests.get('http://localhost:8090/printHelloWorldWeb')
+    return Response(r.iter_lines(chunk_size=1), mimetype="text/json")
 
 
 if __name__ == "__main__":
