ca22678ba86e1668973fe0e89438f31068a38671
Amina <aminaxnavas@gmail.com>
2020-10-23 10:16:41 +0100

    added James' code and connected db to front end

---
+++ DAO_G2.py
@@ -0,0 +1,105 @@
+import mysql.connector
+
+
+def user_auth(username, password):
+    cnx = mysql.connector.connect(user='root', password='ppp', host='127.0.0.1', database='db_grad_cs_1917')
+    cursor1 = cnx.cursor()
+    query = ("SELECT user_pwd FROM users WHERE user_id='" + str(username) + "'")
+    cursor1.execute(query)
+    response = cursor1.fetchone()
+
+    if response:
+        user_pwd = response[0]
+        if user_pwd == password:
+            cursor1.close()
+            cnx.close()
+            return True
+        else:
+            print("invalid username or password")
+            cursor1.close()
+            cnx.close()
+            return False
+        # potentially add login record
+    else:
+        print("invalid username or password")
+        cursor1.close()
+        cnx.close()
+        return False
+
+
+# get the latest and aggregate deal data for an instrument
+def get_instrument_latest_avg(instrument_name):
+    cnx = mysql.connector.connect(user='root', password='ppp', host='127.0.0.1', database='db_grad_cs_1917')
+    cursor2 = cnx.cursor()
+    cursor3 = cnx.cursor()
+
+    query = ("SELECT instrument_id FROM instrument WHERE instrument_name='" + instrument_name + "'")
+    cursor2.execute(query)
+    response = cursor2.fetchone()
+    if response:
+        instrument_id = response[0]
+        print(instrument_id)
+    else:
+        cursor2.close()
+        cnx.close()
+        return "cant find this instrument"
+
+    query = ("SELECT MAX(deal_id) FROM deal ")
+    cursor3.execute(query)
+    latest_deal_id = cursor3.fetchone()[0] - 100
+    print(latest_deal_id)
+
+    query = ("SELECT AVG(deal_amount) FROM deal" + " WHERE deal_type='B' " +
+             " AND deal_instrument_id=" + str(instrument_id) + " AND deal_id>" + str(latest_deal_id))
+    cursor2.execute(query)
+    avg_buy = cursor2.fetchone()[0]
+    print(avg_buy)
+
+    query = ("SELECT AVG(deal_amount) FROM deal" + " WHERE deal_type='S' " +
+             " AND deal_instrument_id=" + str(instrument_id) + " AND deal_id>" + str(latest_deal_id))
+    cursor3.execute(query)
+    avg_sell = cursor3.fetchone()[0]
+    print(avg_sell)
+
+    cursor2.close()
+    cursor3.close()
+    cnx.close()
+    return (avg_sell, avg_buy)
+
+# get the lastest (approximatly 10 to 20) deals from databse
+def get_latest_deals():
+    cnx = mysql.connector.connect(user='root', password='ppp', host='127.0.0.1', database='db_grad_cs_1917')
+    cursor4 = cnx.cursor(buffered=True)
+    cursor5 = cnx.cursor(buffered=True)
+    cursor7 = cnx.cursor()
+    deal_data = []
+    instruments = {}
+
+    query = ("SELECT * FROM instrument")
+    cursor7.execute(query)
+    for row in cursor7:
+        instruments[row[0]] = row[1]
+    # print(instruments)
+    # print(instruments[1001])
+
+    query = ("SELECT MAX(deal_id) FROM deal ")
+    cursor4.execute(query)
+    response = cursor4.fetchone()
+    latest_deal_id = response[0] - 10
+    print(latest_deal_id)
+    query = ("SELECT * FROM deal WHERE deal_id>" + str(latest_deal_id))
+    cursor5.execute(query)
+    # how should return data be formated
+    for row in cursor5:
+        time = row[1]
+        instrument_id = row[3]
+        instrument_type = row[4]
+        price = float(row[5])
+        quantity = row[6]
+        deal = (instruments[instrument_id], price, instrument_type, quantity, time)
+        deal_data.append(deal)
+
+    # cursor4.close()
+    # cursor5.close()
+    cnx.close()
+    return deal_data
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, Response, redirect, url_for
 import json
 import requests
+import DAO_G2
 app = Flask(__name__)
 
 
@@ -8,6 +9,14 @@ def get_data_from_data_gen():
     r = requests.get('http://localhost:8090/getDashboardDisplayDataWeb')
     return json.loads(r.text)
 
+def change_arr_format(arr):
+    if arr[0] is None:
+        arr[0] = 0
+    if arr[1] is None:
+        arr[1] = 0
+    arr[0] = float(arr[0])
+    arr[1] = float(arr[1])
+    return arr
 
 
 @app.route('/')
@@ -22,30 +31,47 @@ def dashboard():
         username = request.form['username']
         password = request.form['password']
 
+        authenticate = DAO_G2.user_auth(username, password)
+
+        if not authenticate:
+            return render_template("error.html")
+
     data = get_data_from_data_gen()
 
     avg_buy_all = data['avg_buy_all']
     avg_sell_all = data['avg_sell_all']
 
-    a_buy_sell = data['a_buy_sell']
-    b_buy_sell = data['b_buy_sell']
-    c_buy_sell = data['c_buy_sell']
-    d_buy_sell = data['d_buy_sell']
-    e_buy_sell = data['e_buy_sell']
-    f_buy_sell = data['f_buy_sell']
-    g_buy_sell = data['g_buy_sell']
-    h_buy_sell = data['h_buy_sell']
-    i_buy_sell = data['i_buy_sell']
-    j_buy_sell = data['j_buy_sell']
-    k_buy_sell = data['k_buy_sell']
-    l_buy_sell = data['l_buy_sell']
+    a_buy_sell = list(DAO_G2.get_instrument_latest_avg("Astronomica"))
+    a_buy_sell = change_arr_format(a_buy_sell)
+    b_buy_sell = list(DAO_G2.get_instrument_latest_avg("Borealis"))
+    b_buy_sell = change_arr_format(b_buy_sell)
+    c_buy_sell = list(DAO_G2.get_instrument_latest_avg("Celestial"))
+    c_buy_sell = change_arr_format(c_buy_sell)
+    d_buy_sell = list(DAO_G2.get_instrument_latest_avg("Deuteronic"))
+    d_buy_sell = change_arr_format(d_buy_sell)
+    e_buy_sell = list(DAO_G2.get_instrument_latest_avg("Eclipse"))
+    e_buy_sell = change_arr_format(e_buy_sell)
+    f_buy_sell = list(DAO_G2.get_instrument_latest_avg("Floral"))
+    f_buy_sell = change_arr_format(f_buy_sell)
+    g_buy_sell = list(DAO_G2.get_instrument_latest_avg("Galactia"))
+    g_buy_sell = change_arr_format(g_buy_sell)
+    h_buy_sell = list(DAO_G2.get_instrument_latest_avg("Heliosphere"))
+    h_buy_sell = change_arr_format(h_buy_sell)
+    i_buy_sell = list(DAO_G2.get_instrument_latest_avg("Interstella"))
+    i_buy_sell = change_arr_format(i_buy_sell)
+    j_buy_sell = list(DAO_G2.get_instrument_latest_avg("Jupiter"))
+    j_buy_sell = change_arr_format(j_buy_sell)
+    k_buy_sell = list(DAO_G2.get_instrument_latest_avg("Koronis"))
+    k_buy_sell = change_arr_format(k_buy_sell)
+    l_buy_sell = list(DAO_G2.get_instrument_latest_avg("Lunatic"))
+    l_buy_sell = change_arr_format(l_buy_sell)
 
     realized_profit = data['realized_profit']
     expected_profit = data['expected_profit']
     realized_loss = data['realized_loss']
     expected_loss = data['expected_loss']
 
-    historical = data['historical']
+    historical = DAO_G2.get_latest_deals()
     deal_data = data['deal_data']
 
     return render_template("dashboard.html", avg_buy_all=avg_buy_all, avg_sell_all=avg_sell_all,
